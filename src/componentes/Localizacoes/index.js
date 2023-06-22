beec79d54b0ccd8ef019930346d43678f0eaace9
Marco Antonio Martins Akerman <marco.akerman@edu.univali.br>
2023-06-22 00:03:52 -0300

    feita a tela de login e atualizado css de localizações, falta só a tela de estoque, procurar como fazer uma tabela

--- src/componentes/Localizacoes/index.js
+++ src/componentes/Localizacoes/index.js
@@ -12,21 +12,21 @@ function Localizacoes() {
             <h2 >Localizações</h2>
             <div>
             <p>
-                Super Carros 1 <br></br>
-                Seg-Sex: 09:00 às 18:00<br></br>
-                Sáb: 09:00 às 12:00<br></br> 
+                Super Carros 1 <br />
+                Seg-Sex: 09:00 às 18:00<br />
+                Sáb: 09:00 às 12:00<br /> <br />
 
-                Rua Intendente João Nunes Vieira, 580<br></br>
-                Ingleses do Rio Vermelho - Florianópolis/SC<br></br>
+                Rua Intendente João Nunes Vieira, 580<br />
+                Ingleses do Rio Vermelho - Florianópolis/SC<br />
             </p>
 
             <p>
-                Super Carros 2<br></br>
-                Seg-Sex: 09:00 às 18:00<br></br>
-                Sáb: 09:00 às 12:00<br></br>
+                Super Carros 2<br />
+                Seg-Sex: 09:00 às 18:00<br />
+                Sáb: 09:00 às 12:00<br /> <br />
 
-                Rod. Armando Calil Bulos, 410<br></br>
-                Vargem Grande - Florianópolis/SC<br></br>
+                Rod. Armando Calil Bulos, 410<br />
+                Vargem Grande - Florianópolis/SC<br />
             </p>
             </div>
             
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,15 @@
+import Botao from '../../componentes/Botao';
+import CampoTexto from '../../componentes/CampoTexto';
 import styles from './Login.module.css';
+
 
 function Login() {
     return (
-        <></>
+        <section className={styles.login}>
+            <CampoTexto obrigatorio={true} label="Login:" placeholder="Login" />
+            <CampoTexto obrigatorio={true} label="Senha:" placeholder="Senha" />
+            <Botao>Entrar</Botao>
+        </section>
     )
 }
 
